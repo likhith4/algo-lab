6a65f4372ec40df04bff8d5fc0af51cd7e760330
likhith <likhithk495@gmail.com>
2024-07-24 14:08:14 +0530

    added  practical files for program 2 and 3

--- prc2.c
+++ prc2.c
@@ -1,9 +1,25 @@
-// to design and implement program to find the minimum cost spanning tree of a given connected undirected graph using prim's algorithm
+// To design and implement program to find the minimum cost spanning tree of a given connected undirected graph using prim's algorithm
+/*The Prim's algorithm build the MST by starting from a selected vertex(the source)and repeatedly adding the smallest edge that connects a vertex in the MST to a vertex outside the MST.
+The process continues untill all vertices are included in the MST.
+*/
 #include<stdio.h>
 #define INF 999
+/*INF: a constant  representing an infinite cost, used to comparison when searching for minimum edge.
+stdio.h : this library includes standard input and output library for I/O operation.
+*/
 int prim(int c[10][10],int n,int s)
 {
 	int v[10],i,j,sum=0,ver[10],d[10],min,u;
+	/*
+	c: a 2D array representing the cost (weight ) of edges between vertices.
+	- n: the number of vertices in the graph
+	- s: the source vertex from which to start building the MST.
+	- v: An array to track whether a vertex is included in the MST(1 if included, 0 otherwise)
+	- sum: A variable to accumulate the total cost of MST.
+	- ver: An array to store the last vertex added to the MST for each vertex.
+	-d : An array to store the minimum cost to connect each vertex to the MST.
+	- min and u: Variables used to find the minimum edge and the corresponding vertex. 
+	*/
 	for(i=1;i<=n;i++)
 	{
 		ver[i]=s;
@@ -11,6 +27,13 @@ int prim(int c[10][10],int n,int s)
 		v[i]=0;
 	}
 	v[s]=1;
+	/*
+	#Initialization:
+	The above loop initializes the arrays:
+	- ver[i] is set to the source vertex for all vertices initially;
+	- d[i] is set to the cost of connecting each vertex to the source vertex
+	- v[i] is intialized to 0, indicating that no vertices are included in the MST to start.
+	*/
 	for(i=1;i<=n-1;i++)
 	{
 		min=INF;
@@ -20,15 +43,28 @@ int prim(int c[10][10],int n,int s)
 				min=d[j];
 				u=j;
 			}
+		/*
+	The above is the main loop to construct the MST.
+	- the outer loop runs n-1 times , as an MST for n vertices contains exactly n-1 edges.
+	- the inner loop searches for the vertex u that is not yet included in the MST (v[j]==0)and has minimim cost to connect to the MST ('d[j]<min').
+	- The minimum cost edge found is stored in the min and corresponding vertex is stored in u.
+	*/
 		v[u]=1;
 		sum=sum+d[u];
 		printf("\n %d -> %d sum=%d",ver[u],u,sum);
+		/*Adding the minimum edge to the MST.
+		- After finding the minimum edge, vertex u is marked included in the MST.
+		- The cost of the edge connecting ver[u](the last vertex added to the MST) to u is added to sum
+		- The program prints the edge added and current total cost of the MST*/
 		for(j=1;j<=n;j++)
 			if(v[j]==0 && c[u][j]<d[j])
 			{
 				d[j]=c[u][j];
 				ver[j]=u;
 			}
+			/*Updating the costs:
+			- This loop updates teh cost array d for each vertex j that is not yet included in the MST.
+			- If the cost of connecting vertex u to vertex j is less than the current minimum cost d[j], it updates and sets ver[j] to u, indicating that the best way to reach j is through u.*/
 	}
 	return sum;
 }
@@ -47,3 +83,10 @@ void main()
 	printf("\nCost=%d",res);
 	getch();
 }
+/*
+- The main function prompts the user for the number of vertices 'n'.
+- It collects the graph data (cost matrix) from the user, where c[i][j] represents the weight of the edge between vertices 'i' and 'j'.
+- The user is then asked to enter the source node 's'
+- The prim() function is called withthe cost matrix, number of vertices and the source node to computer the MST.
+- Finally, it prints the total cost of the MST.
+*/
--- prc3a.c
+++ prc3a.c
@@ -1,10 +1,15 @@
+//Design and implement C/C++ Program to solve All-Pairs Shortest Paths problem using Floyd's algorithm
+/*The Floyd-Warshall algorithm solves the all-pairs shortest path problem by iteratively updating a distance matrix. It considers each vertex as a potential intermediate vertex in the shortest path between two other vertices. The algorithm compares the direct path between two vertices with the path that goes through the intermediate vertex and updates the distance matrix accordingly.*/
 #include<stdio.h>
 #include<conio.h>
 #define INF 999
+/*INF: A constant representing an infinite cost used for comparison when searching for mminimum path.
+The program includes the standard I/O library for basic I/O and conio.h library for console I/O functions.*/
 int min(int a,int b)
 {
     return(a<b)?a:b;
 }
+//The function returns the minimum of two integers 'a' and 'b' using a ternery operator.
 void floyd(int p[][10],int n)
 {
     int i,j,k;
@@ -13,6 +18,15 @@ void floyd(int p[][10],int n)
             for(j=1; j<=n; j++)
                 p[i][j]=min(p[i][j],p[i][k]+p[k][j]);
 }
+/*
+- p: A 2D array representing the cost (weight) of edges between vertices.
+- n: the number of vertices in the graph.
+- The function performs the Floyd warshall algorithm to update the distance matrix 'p'
+1. For each verte k from 1 to n , it considers k as potential intermediate vertex.
+2. For  each pair of vertices i and j it checks if the path from i to j through k is shorter than the current path stored in p[i][j].
+3. If the path through k is shorter it updates p[i][j] with the minimum of the current value and the sum of 'p[i][k]'and p[i][j].
+- After the algorithm completes the distance matrix p contains the minimu costs between all pirs of vertices.
+*/
 void main()
 {
     int a[10][10],n,i,j;
@@ -32,4 +46,10 @@ void main()
     }
     getch();
 }
+/*
+- The main  function prompts the user for the number of vertices 'n'.
+- It collects the graph data (cost matrix ) from the user , where a[i][j] represents the weight of the eedge between vertices i and j . if there is no direct edge between i and j , a[i] is set to INF.
+- The floyd() function is called with the cost matrix and number of vertices to computer the shortest paths between all pairs of vertices.
+- After the algorithm completes, the program prints the sesulting shortest path matrix whre a[i][j] represents the minimum cost to  reach vertex j from vertex i.
 
+*/
